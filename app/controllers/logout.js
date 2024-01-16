e10a35181c545e30a046b79cc8cb7ee84a8d047e
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-16 22:01:59 +0100

    :lock: Improve logout

--- app/controllers/logout.js
+++ app/controllers/logout.js
@@ -2,9 +2,10 @@ const logoutController = {
 
   logoutUser (request, response, next) {
     request.session.destroy();
-    console.log(request.session);
+    // console.log(request.session);
     
     response.clearCookie("connect.sid");
+    response.clearCookie("token");
 
     response.status(200).json({message: "Déconnexion réussie"})
   }
