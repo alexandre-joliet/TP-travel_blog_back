101b7400de39a81dd45718734d7d8e8af0495495
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-16 23:15:44 +0100

    :lock: Update cookie settings

--- server.js
+++ server.js
@@ -10,22 +10,18 @@ app.use(cors({
   // TODO: a modifier par l'url du front
   origin: "http://localhost:3001",
   // origin: "https://tp-travel-blog-front.vercel.app",
-  // origin: "https://api-travel-blog.onrender.com",
   credentials: true,
 }));
 
-/**
-  La session ne doit être utilisable en ce état malgrés la configuration qui est correcte
- * Les appels au BACK se faisant à partir du serveur FRONT, l'utilisateur n'a pas aucune action directe sur la session côté BACK
- */
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   // TODO: Secure à passer en true en prod + revoir la durée du cookie
   cookie: {
-    secure: true,
+    secure: false,
     maxAge: 1000 * 60 * 2,
+    // SameSite: None,
   }
 }));
 
