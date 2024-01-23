58bbe111f7aa1b19d08e24a5c7b3d28b8061f84a
alexandre-joliet <alexandre.joliet@gmail.com>
2024-01-23 15:40:15 +0100

    :coffin: Remove dead code

--- app/routers/categories.js
+++ app/routers/categories.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const categoriesController = require('../controllers/categories');
-// const securityBlog2Service = require('../services/security/security');
 const router = express.Router();
 
-// L'utilisateur est-il connecté
-// router.use(securityBlog2Service.isConnected);
-
-// Categories
-// On récupère toutes les catégories
 router.get('/', categoriesController.getAllCategories)
 
 
